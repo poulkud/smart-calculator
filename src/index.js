2a0e3bd892a01469c5964c0caa6d310efbca1bb8
poulkud <poulkud@gmail.com>
2018-03-13 12:04:55 +0300

    Good

--- src/index.js
+++ src/index.js
@@ -1,26 +1,61 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.stack = [];
+    this.stack.push(initialValue);
   }
 
   add(number) {
-    // your implementation
+    this.stack.push("+" + number);
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.stack.push("-" + number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.stack.push("*" + number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.stack.push("/" + number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.stack.push("^" + number);
+    return this;
+  }
+
+  calculate(){
+    let str = this.stack.join('');
+    var regexp = /(\d+)((\^[0-9])+)+/g;
+    var matchesArray = str.match(regexp);
+    if(matchesArray !== null){
+      for(var i = 0; i < matchesArray.length; i++){
+        var currentRow = matchesArray[i].split('^').map((x) => (Number(x)));
+        var strRow = "";
+        if(currentRow.length == 2 ){
+          strRow = "Math.pow("+currentRow[0]+","+currentRow[1]+")";
+        } else {
+          strRow = "Math.pow(" + currentRow[0];
+          var brackets = ")";
+          for(var j = 1; j < currentRow.length - 1; j++){
+            strRow += " , Math.pow(" + currentRow[j];
+            brackets += ")";
+          }
+          strRow += "," + currentRow[currentRow.length - 1] + brackets
+        }
+        str = str.replace(matchesArray[i], strRow);
+      }
+    }
+    return Number(eval(str)); // Delete Number in case of error
+  }
+
+  toString(){
+    return this.calculate();
   }
 }
 
